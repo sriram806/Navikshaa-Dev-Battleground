8acd9db989c1fb27144f943b7f882451dfa63dd8
sriram806 <boddusriram1234@gmail.com>
2025-05-19 21:38:42 +0530

    Rertify error in server side v1

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ import cors from "cors";
 const app = express();
 const PORT = 5000;
 
-app.use(cors());
 app.use(express.json());
 
 app.use(cors({
@@ -23,7 +22,6 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
-app.options('*', cors());
 
 const slots = Array.from({ length: 9 }, (_, i) => ({
   time: `${10 + i}:00`,
@@ -31,40 +29,40 @@ const slots = Array.from({ length: 9 }, (_, i) => ({
   booked: false,
 }));
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send("Backend is Working!");
 });
 
 app.get('/slots', (req, res) => {
-    res.json(slots);
+    res.status(200).json({success: true, message: "Data Retrived",slots});
 });
 
 app.post('/book', (req, res) => {
   const { time, name } = req.body;
-  if (!time || !name) return res.status(400).json({ error: 'Time and name are required' });
+  if (!time || !name) return res.status(400).json({ success:false, message:"Time and name are required"});
   const slot = slots.find(s => s.time === time);
-  if (!slot) return res.status(404).json({ error: 'Slot not found' });
-  if (slot.booked) return res.status(400).json({ error: 'Slot is already booked' });
+  if (!slot) return res.status(404).json({ success: false,message: 'Slot not found' });
+  if (slot.booked) return res.status(400).json({ success: false, message: 'Slot is already booked' });
 
   slot.booked = true;
   slot.name = name;
   
-  res.json(slot);
+  res.status(200).json({success: true, message: "Successfully Booked your Schudele",slot});
 });
 
 app.post('/cancel', (req, res) => {
   const { time } = req.body;
-  if (!time) return res.status(400).json({ error: 'Time is required' });
+  if (!time) return res.status(400).json({ success: false, message: 'Time is required' });
   const slot = slots.find(s => s.time === time);
-  if (!slot) return res.status(404).json({ error: 'Slot not found' });
+  if (!slot) return res.status(404).json({ success: false, message: 'Slot not found' });
   if (!slot.booked) return res.status(400).json({ error: 'Slot is not booked' });
 
   slot.booked = false;
   slot.name = null;
   
-  res.json(slot);
+  res.status(200).json({ success: true, message: "Successfull cancel your Schudele", slot});
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
